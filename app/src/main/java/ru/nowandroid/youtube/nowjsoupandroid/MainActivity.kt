7631d5d34e9abcd6bf0860031e640900db657713
horaryzero <rnd.programmer.mike@gmail.com>
2020-05-10 13:58:36 +0300

    Added Firebase (connection) + create Realtime Database. Added bottom navigation (do not work because - AndroidX "need fix"). News module disabled

--- app/src/main/java/ru/nowandroid/youtube/nowjsoupandroid/MainActivity.kt
+++ app/src/main/java/ru/nowandroid/youtube/nowjsoupandroid/MainActivity.kt
@@ -2,9 +2,9 @@ package ru.nowandroid.youtube.nowjsoupandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_bottom.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,20 +12,46 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        // SplashScreen
         Thread.sleep(2000)
-
         setTheme(R.style.AppTheme)
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_bottom)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        //Load Fragment
+        loadFragment(ExploreFragment())
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigationExplore -> {
+                    loadFragment(ExploreFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigationDashboard -> {
+                    loadFragment(DashboardFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigationProfile -> {
+                    loadFragment(ProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController,  null)
+    private fun loadFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().also { fragmentTransaction ->
+            fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
+
+
+
+
---
+++ app/src/main/java/ru/nowandroid/youtube/nowjsoupandroid/NewsActivity.kt
@@ -0,0 +1,32 @@
+package ru.nowandroid.youtube.nowjsoupandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class NewsActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Analytics Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Open app")
+        analytics.logEvent("InitScreen", bundle)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,  null)
+    }
+}
