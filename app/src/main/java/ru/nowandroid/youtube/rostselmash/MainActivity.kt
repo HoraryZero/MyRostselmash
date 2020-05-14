cf48680b825176c056f74a5025ca7e2d5998c37e
horaryzero <rnd.programmer.mike@gmail.com>
2020-05-14 22:26:24 +0300

    Deleted Bottom Navigate + all fragments, deleted activity authorization

--- app/src/main/java/ru/nowandroid/youtube/rostselmash/MainActivity.kt
+++ app/src/main/java/ru/nowandroid/youtube/rostselmash/MainActivity.kt
@@ -2,13 +2,7 @@ package ru.nowandroid.youtube.rostselmash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.google.firebase.analytics.FirebaseAnalytics
-import kotlinx.android.synthetic.main.activity_bottom.*
-import ru.nowandroid.youtube.rostselmash.fragments.DashboardFragment
-import ru.nowandroid.youtube.rostselmash.fragments.ExploreFragment
-import ru.nowandroid.youtube.rostselmash.fragments.NotificationFragment
-import ru.nowandroid.youtube.rostselmash.fragments.ProfileFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,34 +20,8 @@ class MainActivity : AppCompatActivity() {
         analytics.logEvent("InitScreen", bundle)
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-
-// Bottom Navigation
-
-//        val exploreFragment = ExploreFragment()
-//        val dashboardFragment = DashboardFragment()
-//        val notificationFragment = NotificationFragment()
-//        val profileFragment = ProfileFragment()
-//
-//        makeCurrentFragment(exploreFragment)
-//
-//        menu_bottom.setOnItemSelectedListener {id ->
-//            when (id) {
-//
-//                R.id.explore -> makeCurrentFragment(exploreFragment)
-//                R.id.dashboard -> makeCurrentFragment(dashboardFragment)
-//                R.id.notifications -> makeCurrentFragment(notificationFragment)
-//                R.id.profile -> makeCurrentFragment(profileFragment)
-//            }
-//            true
-//        }
+        setContentView(R.layout.activity_main)
     }
-
-//    private fun makeCurrentFragment(fragment: Fragment) =
-//            supportFragmentManager.beginTransaction().apply {
-//                replace(R.id.fl_wrapper, fragment)
-//                commit()
-//            }
 }
 
 
