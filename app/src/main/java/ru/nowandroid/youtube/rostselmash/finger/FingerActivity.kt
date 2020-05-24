d5bcda933d113b41aec1328c6d7042ea8475f497
horaryzero <rnd.programmer.mike@gmail.com>
2020-05-24 14:19:25 +0300

    Start Fingerprint Implementation

---
+++ app/src/main/java/ru/nowandroid/youtube/rostselmash/finger/FingerActivity.kt
@@ -0,0 +1,14 @@
+package ru.nowandroid.youtube.rostselmash.finger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.nowandroid.youtube.rostselmash.R
+
+class FingerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finger)
+    }
+}
+
